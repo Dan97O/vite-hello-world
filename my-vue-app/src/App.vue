19f60b4e5f42dabbb62d2d2cc754e045c8bf6f8c
Dan97O <dan97-oddo@live.it>
2023-04-12 14:12:00 +0200

    initial commit and message 'hello vite'

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data(){
+    return {
+      message: 'Hello Vite and Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style >
+</style>
